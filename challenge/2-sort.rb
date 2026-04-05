530273569c7c8b11c601a492bced977a6eac0e55
gulay-yusifli <yusifligulay06@gmail.com>
2026-04-05 13:33:33 +0000

    task

--- challenge/2-sort.rb
+++ challenge/2-sort.rb
@@ -1,32 +1,29 @@
+#!/usr/bin/env ruby
 ###
 #
-#  Sort integer arguments (ascending) 
+# 2. Sort
 #
 ###
 
 result = []
-ARGV.each do |arg|
-    # skip if not integer
-    next if arg !~ /^-?[0-9]+$/
 
-    # convert to integer
-    i_arg = arg.to_i
-    
-    # insert result at the right position
-    is_inserted = false
-    i = 0
-    l = result.size
-    while !is_inserted && i < l do
-        if result[i] < i_arg
-            i += 1
-        else
-            # result.insert(i - 1, i_arg)
-            result.insert(i, i_arg)
-            is_inserted = true
-            break
-        end
+ARGV.each do |arg|
+  # skip if not an integer
+  next unless arg =~ /^-?\d+$/
+
+  i_arg = arg.to_i
+  is_inserted = false
+
+  result.each_with_index do |val, i|
+    if i_arg < val
+      result.insert(i, i_arg)   # müsbət indekslə insert
+      is_inserted = true
+      break
     end
-    result << i_arg if !is_inserted
+  end
+
+  # əgər insert olunmayıbsa, array-in sonuna əlavə et
+  result << i_arg unless is_inserted
 end
 
 puts result
